1fe7159ac3a4e290c27fefe8b4c1806d5ff13f96
진아영 <ay8160@daum.net>
2025-01-13 18:22:17 +0900

    [BOJ] 컴백홈 / 실버1

--- yyong/2025.01/240112_컴백홈.py
+++ yyong/2025.01/240112_컴백홈.py
@@ -12,21 +12,23 @@ result = 0
 def move(r, c, k):
     global result, R, C, K, visited
 
+    # 조건 맞았을 때 result += 1
     if (r, c) == (0, C-1) and k == K:
         result += 1
         return
 
+    # 제한 거리보다 많이 걸렸을 때 return
     if k > K:
         return
 
     for di, dj in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
         nr, nc = r + di, c + dj
 
+        # 백트래킹
         if 0 <= nr < R and 0 <= nc < C and not visited[nr][nc] and arr[nr][nc] != 'T':
             visited[nr][nc] = True
             move(nr, nc, k+1)
             visited[nr][nc] = False
-
 
 move(R-1, 0, 1)
 
