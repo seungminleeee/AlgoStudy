1b47c2e4a1df54f5802243a771a9090d52b768d4
c4526 <c4526@naver.com>
2025-01-13 23:37:32 +0900

    [BOJ] 타노스 / 실버3

--- joy/2025.01/250113_타노스.py
+++ joy/2025.01/250113_타노스.py
@@ -1,4 +1,5 @@
 # 1은 앞에서부터 0은 뒤에서 부터 빼주면 된다..
+# 카운트도 딕셔너리 말고 count 로 쉽게 바꿨음
 string = list(input())
 zero_cnt = string.count('0') // 2
 one_cnt = string.count('1') // 2
@@ -12,25 +13,25 @@ for j in range(zero_cnt):
 print(''.join(string))
 
 
+# 실패작 ;;; 
+# 처음엔 그냥 갯수만큼 하고 작은 수 만드는 줄 알고 .. 이러니까 25점 맞음 ㅋㅋ;;
+string = input()
+dict = {}
+for i in string:
+    if i in dict:
+        dict[i] += 1
+    else:
+        dict[i] = 1
 
+zero = 0
+one = 0
 
-# string = input()
-# dict = {}
-# for i in string:
-#     if i in dict:
-#         dict[i] += 1
-#     else:
-#         dict[i] = 1
+for i in dict:
+    if i == '1':
+        one = dict['1'] // 2
+    elif i == '0':
+        zero = dict['0'] //2
 
-# zero = 0
-# one = 0
-
-# for i in dict:
-#     if i == '1':
-#         one = dict['1'] // 2
-#     elif i == '0':
-#         zero = dict['0'] //2
-
-# print('0'*zero+'1'*one)
+print('0'*zero+'1'*one)
 
 
