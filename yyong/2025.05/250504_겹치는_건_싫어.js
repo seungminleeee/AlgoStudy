ef4c1e59fe91028081e2525481f24bfeac5c3779
진아영 <ay8160@daum.net>
2025-05-06 15:42:28 +0900

    [BOJ] 겹치는 건 싫어 / 실버1

--- yyong/2025.05/250504_겹치는_건_싫어.js
+++ yyong/2025.05/250504_겹치는_건_싫어.js
@@ -14,10 +14,12 @@ numberCount.set(sequence[left], 1);
 
 let result = 1;
 
+// 수열 범위 체크
 for (let right = 1; right < N; right++) {
   const rightNum = sequence[right];
   numberCount.set(rightNum, (numberCount.get(rightNum) || 0) + 1);
 
+  // 조건 맞을 때 까지 범위 조절
   while (numberCount.get(rightNum) > M) {
     const leftNum = sequence[left++];
     numberCount.set(leftNum, numberCount.get(leftNum) - 1);
